57bb0b5c40a2d71f2fd7446704454e9b6fe82fa2
chibimayura <chibimayura@gmail.com>
2018-08-16 15:00:11 -0700

    fix department_id to fill in products according to departments.department_id

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -4,14 +4,35 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
+CREATE TABLE departments(
+	department_id INT NOT NULL AUTO_INCREMENT,
+	department_name VARCHAR(255) NOT NULL,
+	over_head_costs DECIMAL(10,2) NOT NULL DEFAULT 0,
+	PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES
+	("Food", 20),
+	("Camera", 1000),
+	("Electronics", 10),
+	("Books", 4),
+	("Oral Hygiene", 10),
+	("Organ", 10000),
+	("Toys", 400),
+	("Furniture", 2000),
+	("Movies", 6);
+
 CREATE TABLE products(
 	item_id INT NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(255) NOT NULL,
 	department_name VARCHAR(255) NOT NULL,
+	department_id INT,
 	price DECIMAL(10,2) NOT NULL,
 	stock_quantity INT(10) NOT NULL,
 	product_sales INT(10) NOT NULL DEFAULT 0,
-	PRIMARY KEY(item_id)
+	PRIMARY KEY(item_id),
+	FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
@@ -27,22 +48,7 @@ VALUES
 	("Perfect Blue", "Movies", 12.99, 10000),
 	("Bionic Right Leg", "Electronics", 70000.99, 39);
 
--- CREATE TABLE departments(
--- 	department_id INT NOT NULL AUTO_INCREMENT,
--- 	department_name VARCHAR(255) NOT NULL,
--- 	over_head_costs DECIMAL(10,2) NOT NULL DEFAULT 0,
--- 	PRIMARY KEY (department_id),
--- 	FOREIGN KEY (department_name) REFERENCES products(department_name) 	
--- );
-
--- INSERT INTO departments (department_name, over_head_costs)
--- VALUES
--- 	("Food", 20),
--- 	("Camera", 1000),
--- 	("Electronics", 10),
--- 	("Books", 4),
--- 	("Oral Hygiene", 10),
--- 	("Organ", 10000),
--- 	("Toys", 400),
--- 	("Furniture", 2000),
--- 	("Movies", 6);
+UPDATE products 
+INNER JOIN departments 
+ON (departments.department_name = products.department_name) 
+SET products.department_id = departments.department_id;
