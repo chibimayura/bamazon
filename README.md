a6e396d1a2e57163ee8c23b4e88ea23e17545674
chibimayura <chibimayura@gmail.com>
2018-08-15 20:57:19 -0700

    supervisor still work in progress, updating readme file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # bamazon
+
+Bamazon is an Amazon-like storefront with MySQL and Node. This app will allow customers to make purchases and deplete stock from the store's inventory.
+
+## REQUIREMENTS
+
+#### 1. Installation
+After cloning this repository, in your Bash terminal, enter:
+```
+npm install
+```
+This should install the three packages required for this node,
+1. cli-table - for cleanier view on terminal
+2. inquirer - for prompting questions for user
+3. mysql - for accessing database
+
+#### 2. File Changes
+In the `bamazonCustomer.js`, `bamazonManager.js`, and `bamazonSupervisor.js` you will need to change `line 7` the `var sqlPW = [insert your mySql password];`.
+
+#### 3. Add Database
+
+In your mySql terminal add the information in the file `bamazon_db.sql`.
+
+## HOW TO USE
+1. 
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -12,15 +12,14 @@ CREATE TABLE products(
 	price DECIMAL(10,2) NOT NULL,
 	stock_quantity INT(10) NOT NULL,
 	product_sales INT(10) NOT NULL DEFAULT 0,
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
 
 CREATE TABLE departments(
-	id INT NOT NULL AUTO_INCREMENT,
-	department_id INT NOT NULL,
+	department_id INT NOT NULL AUTO_INCREMENT,
 	department_name VARCHAR(255) NOT NULL,
-	over_head_costs DECIMAL(10,2) DEFAULT 0,
-	PRIMARY KEY (id),
+	over_head_costs DECIMAL(10,2) NOT NULL DEFAULT 0,
+	PRIMARY KEY (department_id),
 	FOREIGN KEY (department_name) REFERENCES products(department_name) 	
 );
 
@@ -28,11 +27,23 @@ INSERT INTO products (item_id, product_name, department_name, price, stock_quant
 VALUES 
 	(01, "Bananas", "Food", 0.69, 1000000),
 	(02, "Panasonic Lumix DC-GH5", "Camera", 2397.99, 10),
-	(03, "Phone Charger compatiable with android and iphone", "Cellphone accessories", 20.99, 50),
+	(03, "Phone Charger compatiable with android and iphone", "Electronics", 20.99, 50),
 	(04, "All My Friends Are Dead", "Books", 8.96, 1250),
 	(05, "Universal Organic Vegan Gluten-Free BPA-Free Cruelty-Free Manual Toothbrush", "Oral Hygiene", 20.99, 3000),
 	(06, "Liver", "Organ", 557100.00, 5),
 	(07, "Life-sized Magikarp", "Toys", 800.99, 20),
 	(08, "Mansion-sized Mattress", "Furniture", 5561.89, 15),
 	(09, "Perfect Blue", "Movies", 12.99, 10000),
-	(10, "Bionic Right Leg", "Robotics", 70000.99, 39);
+	(10, "Bionic Right Leg", "Electronics", 70000.99, 39);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES
+	("Food", 20),
+	("Camera", 1000),
+	("Electronics", 10),
+	("Books", 4),
+	("Oral Hygiene", 10),
+	("Organ", 10000),
+	("Toys", 400),
+	("Furniture", 2000),
+	("Movies", 6);
